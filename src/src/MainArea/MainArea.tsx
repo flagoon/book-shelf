e16d58412bbc589af52acdc3d80840dfadf9ac35
Paweł Kochanek <pawel.kochanek@avid.com>
2019-01-11 09:39:06 +0100

    Change props to get them from store.

--- src/src/MainArea/MainArea.tsx
+++ src/src/MainArea/MainArea.tsx
@@ -1,36 +1,31 @@
-import * as React from 'react'
-import AddBookForm from './AddBookForm/AddBookForm'
-import Bookshelf from './Bookshelf/Bookshelf'
-import Archive from './Archive/Archive'
+import * as React from 'react';
 import { connect } from 'react-redux';
+import AddBookForm from './AddBookForm/AddBookForm';
+import Archive from './Archive/Archive';
+import Bookshelf from './Bookshelf/Bookshelf';
 
 interface IProps {
     activePage: string;
 }
 
-type eVisibleComponent = {
+interface IVisibleComponent {
     bookShelf: JSX.Element;
     addBookForm: JSX.Element;
     archive: JSX.Element;
     [key: string]: JSX.Element;
-  }
+}
 
-const MainArea = (props: IProps) => {
-    const {activePage} = props;
-    const visibleComponent: eVisibleComponent = {
+const MainArea = ({ activePage }: IProps) => {
+    const visibleComponent: IVisibleComponent = {
         bookShelf: <Bookshelf />,
         addBookForm: <AddBookForm />,
-        archive: <Archive />
-    }
-    return (visibleComponent[activePage])
-}
+        archive: <Archive />,
+    };
+    return (visibleComponent[activePage]);
+};
 
 const mapStateToProps = (state: any) => ({
-    activePage: state.activePage
-})
+    activePage: state.page.activePage,
+});
 
-const mapDispatchToState = {
-    
-}
-
-export default connect(mapStateToProps)(MainArea)
+export default connect(mapStateToProps, undefined)(MainArea);
