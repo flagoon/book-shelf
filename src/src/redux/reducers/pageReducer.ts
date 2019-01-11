26f697314099d72b69441abc2cc3fa4dc2e779b6
Paweł Kochanek <pawel.kochanek@avid.com>
2019-01-11 09:40:43 +0100

    Change file name.

---
+++ src/src/redux/reducers/pageReducer.ts
@@ -0,0 +1,14 @@
+import actionTypes from '../actionTypes';
+
+const initialState = {
+    activePage: 'bookShelf',
+};
+
+export const pageReducer = (state = initialState, action: any) => {
+    switch (action.type) {
+        case actionTypes.CHANGE_ACTIVE_PAGE:
+            return action.payload;
+        default:
+            return state;
+    }
+};
