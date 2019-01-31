f734838ae7add8c5f828e2c986b42025e00b659f
Paweł Kochanek <pawel.kochanek@gmail.com>
2019-01-31 22:42:01 +0100

    Created view for Bookshelf
    Prepare advanced view for bookshelf.
    
    Working on Bookcase component
    
    ButtonGenerator component WIP
    
    Created button generator.
    
    Added types for styled-components.
    
    Working on styles for button.
    
    View for BookshelfCase

---
+++ src/Components/Button/Button.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface IButton {
+    primary: boolean;
+    alert: boolean;
+    success: boolean;
+    value: string;
+    className: string;
+    [key: string]: boolean | string;
+}
+
+export const Button = ({ value, className }: IButton) => {
+    return <button className={className}>{value}</button>;
+};
+
+export const StyledButton = styled(Button)`
+    background-color: ${({ alert, primary, success }: IButton) =>
+        (primary && 'blue') || (alert && 'red') || (success && 'green') || 'grey'};
+    color: white;
+    font-weight: bold;
+    padding: 0.5rem;
+    border: 0;
+    border-radius: 5px;
+    margin-right: 0.5rem;
+
+    &:last-child {
+        margin-right: 0;
+    }
+
+    &:hover {
+        cursor: pointer;
+        background-color: ${({ alert, primary, success }: IButton) =>
+            (primary && 'darkblue') || (alert && 'darkred') || (success && 'darkgreen') || 'grey'};
+    }
+`;
--- src/Components/MainArea/Bookshelf/Bookcase/Bookcase.tsx
+++ src/Components/MainArea/Bookshelf/Bookcase/Bookcase.tsx
@@ -1,24 +1,51 @@
 import React from 'react';
-import styled from 'styled-components';
-import { IBooks } from '../../../../typings/IBooks';
+import { IBook } from '../../../../typings/IBooks';
+import { StyledButton } from '../../../Button/Button';
+import {
+    Author,
+    BookcaseHolder,
+    BookDescription,
+    ControlButtons,
+    DataWrapper,
+    TechnicalBookData,
+    TextDataWrapper,
+    Title,
+} from './Bookcase.styled';
 
 interface IProps {
-    book: IBooks;
+    book: IBook;
 }
 
 export const Bookcase = ({ book }: IProps) => {
     return (
-        <div>
-            <ul>
-                <li>{book.title}</li>
-                <li>{book.author}</li>
-                <li>{book.isbn}</li>
-                <li>{book.cover}</li>
-                <li>{book.date}</li>
-                <li>{book.pages}</li>
-                <li>{book.description}</li>
+        <BookcaseHolder>
+            <DataWrapper>
                 <img src={book.picture} />
-            </ul>
-        </div>
+                <TextDataWrapper>
+                    <Title>{book.title}</Title>
+                    <Author>Author: {book.author}</Author>
+                    <TechnicalBookData>
+                        <li>
+                            <span>ISBN:</span> {book.isbn},
+                        </li>
+                        <li>
+                            <span>cover:</span> {book.cover},
+                        </li>
+                        <li>
+                            <span>acquired:</span> {book.date},
+                        </li>
+                        <li>
+                            <span>pages:</span> {book.pages}
+                        </li>
+                    </TechnicalBookData>
+                    <BookDescription>{book.description}</BookDescription>
+                    <ControlButtons>
+                        <StyledButton success value='Mark as read' className='styledButton' />
+                        <StyledButton primary value='Archive the book' className='styledButton' />
+                        <StyledButton alert value='Delete the book' className='styledButton' />
+                    </ControlButtons>
+                </TextDataWrapper>
+            </DataWrapper>
+        </BookcaseHolder>
     );
 };
