24847157698791c4640203eeaa3855fbe952af70
Paweł Kochanek <pawel.kochanek@gmail.com>
2019-02-05 21:53:04 +0100

    Single book view

--- src/Components/MainArea/Archive/ArchivedBook/ArchivedBook.tsx
+++ src/Components/MainArea/Archive/ArchivedBook/ArchivedBook.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 import { IBook } from '../../../../typings/IBooks';
 import { Author, BookRow, ISBN, Title } from './ArchivedBook.styled';
 
@@ -9,11 +11,23 @@ interface IArchivedBook {
 export const ArchivedBook = (props: IArchivedBook) => {
     const { book } = props;
     return (
-        <BookRow>
-            <Title>{book.title}</Title>
-            <Author>{book.author}</Author>
-            <ISBN>{book.isbn}</ISBN>
-            <div>'isRead'</div>
-        </BookRow>
+        <StyledNavLink to={`/archive/${book.id}`}>
+            <BookRow>
+                <Title>{book.title}</Title>
+                <Author>{book.author}</Author>
+                <ISBN>{book.isbn}</ISBN>
+            </BookRow>
+        </StyledNavLink>
     );
 };
+
+const StyledNavLink = styled(NavLink)`
+    color: black;
+    text-decoration: none;
+    margin-bottom: 1px;
+
+    &:hover {
+        color: blue;
+        text-decoration: underline;
+    }
+`;
