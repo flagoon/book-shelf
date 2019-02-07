86e196e2f983210b560345ac3487220ff66dfd08
Paweł Kochanek <pawel.kochanek@gmail.com>
2019-02-07 08:08:40 +0100

    Skeleton of ContextAPI.

--- src/Components/MainArea/AddBookForm/AddBookForm.tsx
+++ src/Components/MainArea/AddBookForm/AddBookForm.tsx
@@ -1,93 +1,12 @@
-import { Field, Formik, FormikProps } from 'formik';
 import React from 'react';
 import { BooksConsumer } from '../../../ContextProvider/BooksProvider';
-import { convertDateForInputField } from '../../../Utils/DateHelpers';
-import { StyledButton } from '../../Common/Button/Button';
-import Tooltip from '../../Common/Tooltip/Tooltip';
-import { ButtonContainer, StyledForm, StyledLabel } from './AddBookForm.styled';
-import bookValidationSchema from './bookValidationSchema';
 
-interface IAddBookFormProps {
-    hideNotification: () => void;
-}
-
-type ICover = 'hard' | 'soft';
-
-export interface IFormikValues {
-    title: string;
-    author: string;
-    isbn: string;
-    description: string;
-    isRead: boolean;
-    picture: string;
-    pages: number;
-    cover: ICover;
-    date: any;
-}
-
-const AddBookForm = ({ hideNotification }: IAddBookFormProps) => {
+const AddBookForm = () => {
     return (
         <BooksConsumer>
-            {({ onNewBookFormSubmit }) => (
-                <Formik
-                    initialValues={{
-                        title: '',
-                        author: '',
-                        isbn: '',
-                        description: '',
-                        isRead: false,
-                        picture: '',
-                        pages: 0,
-                        cover: 'soft',
-                        date: convertDateForInputField(new Date()),
-                    }}
-                    onSubmit={(values: IFormikValues) => {
-                        hideNotification();
-                        onNewBookFormSubmit(values);
-                    }}
-                    validationSchema={bookValidationSchema}
-                    render={({ values, errors, touched }: FormikProps<IFormikValues>) => (
-                        <StyledForm>
-                            <StyledLabel
-                                htmlFor={'title'}
-                                color={errors.title && touched.title ? 'red' : 'black'}
-                            >
-                                Title
-                            </StyledLabel>
-                            <Field name="title" value={values.title} />
-                            {errors.title && touched.title && <Tooltip type={'error'}>{errors.title}</Tooltip>}
-                            <StyledLabel htmlFor={'author'}>Author</StyledLabel>
-                            <Field name="author" value={values.author} />
-                            <StyledLabel htmlFor={'isbn'}>ISBN</StyledLabel>
-                            <Field name="isbn" value={values.isbn} />
-                            <StyledLabel htmlFor={'description'}>Description</StyledLabel>
-                            <Field name="description" component={'textarea'}>
-                                {values.description}
-                            </Field>
-                            <StyledLabel htmlFor={'isRead'} className={'labelIsRead'}>
-                                Is read?
-                                <Field name="isRead" id={'isRead'} checked={values.isRead} type={'checkbox'} />
-                            </StyledLabel>
-                            <StyledLabel htmlFor={'picture'}>Picture (URL)</StyledLabel>
-                            <Field name="picture" value={values.picture} type={'url'} />
-                            <StyledLabel htmlFor={'pages'}>Pages</StyledLabel>
-                            <Field name="pages" value={values.pages} type={'number'} />
-                            <StyledLabel htmlFor={'cover'}>Cover</StyledLabel>
-                            <Field name="cover" default={values.cover} component={'select'}>
-                                <option value={'hard'} label={'Hard'} />
-                                <option value={'soft'} label={'Soft'} />
-                            </Field>
-                            <StyledLabel htmlFor={'date'}>Acquired</StyledLabel>
-                            <Field name="date" value={values.date} type={'date'} />
-                            <ButtonContainer>
-                                <StyledButton type={'submit'} color={'blue'}>
-                                    Submit
-                                </StyledButton>
-                            </ButtonContainer>
-                        </StyledForm>
-                    )}
-                />
-            )}
+            {context => {
+                return <div>{context.one}</div>;
+            }}
         </BooksConsumer>
     );
 };
--- src/ContextProvider/BooksProvider.tsx
+++ src/ContextProvider/BooksProvider.tsx
@@ -1,148 +1,16 @@
-import { Map } from 'immutable';
-import React, { FormEvent } from 'react';
-import { IFormikValues } from '../Components/MainArea/AddBookForm/AddBookForm';
-import { IBook } from '../typings/IBooks';
+import React from 'react';
 
-export interface IBookContext extends IBooksProviderState {
-    changeReadStatus: (id: string) => void;
-    changeArchiveStatus: (id: string) => void;
-    deleteBook: (id: string) => void;
-    onMarkPageSubmit: (event: FormEvent<HTMLFormElement>) => void;
-    onMarkPageChange: (event: FormEvent<HTMLInputElement>, id: string) => void;
-    onNewBookFormSubmit: (values: IFormikValues) => void;
-}
+const defaultValue = { one: 1, two: 2 };
+const { Provider, Consumer } = React.createContext(defaultValue);
 
-interface IBooksProviderState {
-    books: Map<string, IBook>;
-    tempMarkedPage: number;
-    tempBookId: string;
-}
-
-const defaultValue = {
-    changeReadStatus: () => undefined,
-    changeArchiveStatus: () => undefined,
-    deleteBook: () => undefined,
-    onMarkPageSubmit: () => undefined,
-    onMarkPageChange: () => undefined,
-    onNewBookFormSubmit: () => undefined,
-    books: Map({
-        '1': {
-            id: '1',
-            author: 'author',
-            index: 1,
-            picture: '',
-            pages: 0,
-            cover: 'hard',
-            isbn: '',
-            date: '',
-            title: '',
-            description: '',
-            isRead: true,
-            isArchived: true,
-            markedPage: 0,
-        },
-    }),
-    tempMarkedPage: 0,
-    tempBookId: '',
-};
-const { Provider, Consumer } = React.createContext<IBookContext>(defaultValue);
-
-class BooksProvider extends React.Component<{}, IBooksProviderState> {
-    public static getBooks(): IBook[] {
-        return require('../mockedData/books.json');
-    }
-
-    constructor(props: {}) {
-        super(props);
-        this.state = {
-            books: defaultValue.books,
-            tempMarkedPage: defaultValue.tempMarkedPage,
-            tempBookId: defaultValue.tempBookId,
-        };
-        this.changeReadStatus = this.changeReadStatus.bind(this);
-        this.changeArchiveStatus = this.changeArchiveStatus.bind(this);
-        this.deleteBook = this.deleteBook.bind(this);
-        this.onMarkPageSubmit = this.onMarkPageSubmit.bind(this);
-        this.onMarkPageChange = this.onMarkPageChange.bind(this);
-        this.onNewBookFormSubmit = this.onNewBookFormSubmit.bind(this);
-    }
-
-    public componentDidMount() {
-        this.setState({
-            books: this.prepareMapForState(BooksProvider.getBooks()),
-        });
-    }
-
-    public changeArchiveStatus(id: string) {
-        const selectedBook = this.state.books.get(id);
-        if (selectedBook) {
-            selectedBook.isArchived = !selectedBook.isArchived;
-            const newBooks = this.state.books.set(id, selectedBook);
-            this.setState({ books: newBooks });
-        }
-    }
-
-    public changeReadStatus(id: string) {
-        const selectedBook = this.state.books.get(id);
-        if (selectedBook) {
-            selectedBook.isRead = !selectedBook.isRead;
-            const newBooks = this.state.books.set(id, selectedBook);
-            this.setState({ books: newBooks });
-        }
-    }
-
-    public deleteBook(id: string) {
-        const reducedBooks = this.state.books.delete(id);
-        this.setState({ books: reducedBooks });
-    }
-
-    public onMarkPageSubmit(e: FormEvent<HTMLFormElement>) {
-        e.preventDefault();
-        if (this.state.tempBookId) {
-            const book = this.state.books.get(this.state.tempBookId);
-            const newBook: IBook = { ...(book as IBook), markedPage: this.state.tempMarkedPage };
-            const newBooks = this.state.books.set(this.state.tempBookId, newBook);
-            this.setState({ books: newBooks });
-        }
-    }
-
-    public onMarkPageChange(e: FormEvent<HTMLInputElement>, id: string) {
-        e.preventDefault();
-        const target = e.target as HTMLInputElement;
-        this.setState({ ...this.state, tempBookId: id, tempMarkedPage: parseInt(target.value, 10) });
-    }
-
-    public onNewBookFormSubmit(values: IFormikValues) {
-        alert(JSON.stringify(values, undefined, 2));
-    }
-
-    public render() {
-        return (
-            <Provider
-                value={{
-                    books: this.state.books,
-                    tempMarkedPage: this.state.tempMarkedPage,
-                    tempBookId: this.state.tempBookId,
-                    changeReadStatus: this.changeReadStatus,
-                    changeArchiveStatus: this.changeArchiveStatus,
-                    deleteBook: this.deleteBook,
-                    onMarkPageSubmit: this.onMarkPageSubmit,
-                    onMarkPageChange: this.onMarkPageChange,
-                    onNewBookFormSubmit: this.onNewBookFormSubmit,
-                }}
-            >
-                {this.props.children}
-            </Provider>
-        );
-    }
-
-    public prepareMapForState = (books: IBook[]): Map<string, IBook> => {
-        let preparedMappedBooks: Map<string, IBook> = Map();
-        books.forEach((book) => {
-            preparedMappedBooks = preparedMappedBooks.set(book.id, book);
-        });
-        return preparedMappedBooks;
+class BooksProvider extends React.Component<{}, { one: number; two: number }> {
+    public state = {
+        one: 1,
+        two: 2,
     };
+    public render() {
+        return <Provider value={{ one: this.state.one, two: this.state.two }}>{this.props.children}</Provider>;
+    }
 }
 
 export { BooksProvider, Consumer as BooksConsumer };
