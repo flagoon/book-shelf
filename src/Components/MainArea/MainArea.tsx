f1b00d91fac3bf8a3af1eee9d1b30174e52422f9
Paweł Kochanek <pawel.kochanek@gmail.com>
2019-02-07 23:01:38 +0100

    Refactor for using ContextApi

--- src/Components/MainArea/MainArea.tsx
+++ src/Components/MainArea/MainArea.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import ArchiveContainer from '../../Containers/MainArea/Archive/ArchiveContainer';
-import BookshelfContainer from '../../Containers/MainArea/Bookshelf/BookshelfContainer';
-import { BookContainer } from '../../Containers/MainArea/SingleBook/BookContainer';
 import { NoMatch } from '../ErrorPages/404_page';
 import AddBookForm from './AddBookForm/AddBookForm';
+import Archive from './Archive/Archive';
+import Bookshelf from './Bookshelf/Bookshelf';
 import { StyledMainArea } from './MainArea.styled';
+import { SingleBook } from './SingleBook/SingleBook';
 
 const MainArea = () => {
     return (
         <StyledMainArea>
             <Switch>
-                <Route exact path='/' component={BookshelfContainer} />
-                <Route path='/bookshelf' component={BookshelfContainer} />
-                <Route exact path='/archive' component={ArchiveContainer} />
+                <Route exact path='/' component={Bookshelf} />
+                <Route path='/bookshelf' component={Bookshelf} />
+                <Route exact path='/archive' component={Archive} />
                 <Route path='/addbook' component={AddBookForm} />
-                <Route path='/archive/:bookId' component={BookContainer} />
+                <Route path='/archive/:bookId' component={SingleBook} />
                 <Route component={NoMatch} />
             </Switch>
         </StyledMainArea>
