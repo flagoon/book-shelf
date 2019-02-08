4042c9f444865b6e47d40cd19ab3205b417f91a2
Paweł Kochanek <pawel.kochanek@avid.com>
2019-02-08 13:14:33 +0100

    Corrections for types and style.

--- src/Components/MainArea/Bookshelf/Bookcase/Bookcase.tsx
+++ src/Components/MainArea/Bookshelf/Bookcase/Bookcase.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
-import { ShowNotificationType } from '../../../../ContextProvider/NotificationProvider';
 import { IBook } from '../../../../typings/IBooks';
-import { StyledButton } from '../../../Common/Button/Button';
-import EmojiGenerator from '../../../Common/EmojiGenerator/EmojiGenerator';
+import { StyledButton } from '../../../Button/Button';
 import {
     Author,
     BookcaseHolder,
@@ -18,44 +14,16 @@ import {
 
 interface IBookcaseProps {
     book: IBook;
-    changeReadStatus?: (id: string) => void;
-    changeArchiveStatus?: (id: string) => void;
-    deleteBook?: (id: string) => void;
-    showNotification: (type: ShowNotificationType, bookId?: string) => void;
 }
 
 export const Bookcase = (props: IBookcaseProps) => {
-    const { book, changeReadStatus, changeArchiveStatus, deleteBook, showNotification } = props;
-
-    const handleBookDelete = (e: MouseEvent): void => {
-        e.stopPropagation();
-        showNotification('DELETE');
-        deleteBook!(book.id);
-    };
-    const handleBookArchive = (e: MouseEvent): void => {
-        e.stopPropagation();
-        showNotification('ARCHIVE');
-        changeArchiveStatus!(book.id);
-    };
-    const handleBookRead = (e: MouseEvent): void => {
-        e.stopPropagation();
-        changeReadStatus!(book.id);
-    };
-
-    const handlePageMark = (e: MouseEvent): void => {
-        e.stopPropagation();
-        showNotification('MARK_PAGE', book.id);
-    };
-
+    const { book } = props;
     return (
         <BookcaseHolder>
             <DataWrapper>
-                <img src={book.picture} alt="Cover image" />
+                <img src={book.picture} alt='Cover image'/>
                 <TextDataWrapper>
-                    <Title>
-                        <StyledLink to={`/book/${book.id}`}>{book.title}</StyledLink>
-                        {book.isRead && <EmojiGenerator value="✔️" label="This book is read." />}
-                    </Title>
+                    <Title>{book.title}</Title>
                     <Author>Author: {book.author}</Author>
                     <TechnicalBookData>
                         <li>
@@ -68,56 +36,17 @@ export const Bookcase = (props: IBookcaseProps) => {
                             <span>acquired:</span> {book.date},
                         </li>
                         <li>
-                            <span>pages:</span> {book.pages},
-                        </li>
-                        <li>
-                            <span>on page:</span> {book.markedPage},
+                            <span>pages:</span> {book.pages}
                         </li>
                     </TechnicalBookData>
                     <BookDescription>{book.description}</BookDescription>
                     <ControlButtons>
-                        {book.isArchived ? (
-                            <>
-                                <Link to={'/bookshelf'} style={{ marginRight: '0.5rem' }}>
-                                    <StyledButton color="red" onClick={handleBookDelete}>
-                                        <EmojiGenerator value="🗑" label="Delete the book" />
-                                        Delete the book
-                                    </StyledButton>
-                                </Link>
-                                <StyledButton color="limeGreen" onClick={handleBookArchive}>
-                                    <EmojiGenerator value="💾" label="Archive the book" />
-                                    Read again
-                                </StyledButton>
-                            </>
-                        ) : (
-                            <>
-                                <StyledButton color="blue" onClick={handlePageMark}>
-                                    <EmojiGenerator value="📖" label="Mark the page" />
-                                    Mark the page
-                                </StyledButton>
-                                <StyledButton color="red" onClick={handleBookArchive}>
-                                    <EmojiGenerator value="💾" label="Archive the book" />
-                                    Archive the book
-                                </StyledButton>
-                                <StyledButton color="green" onClick={handleBookRead}>
-                                    <EmojiGenerator value="🔖" label="Mark as read" />
-                                    Mark as read
-                                </StyledButton>
-                            </>
-                        )}
+                        <StyledButton success value='Mark as read' className='styledButton' />
+                        <StyledButton primary value='Archive the book' className='styledButton' />
+                        <StyledButton alert value='Delete the book' className='styledButton' />
                     </ControlButtons>
                 </TextDataWrapper>
             </DataWrapper>
         </BookcaseHolder>
     );
 };
-
-const StyledLink = styled(Link)`
-    text-decoration: none;
-    color: black;
-
-    &:hover {
-        background-color: darkblue;
-        color: white;
-    }
-`;
--- src/Components/MainArea/Bookshelf/Bookshelf.tsx
+++ src/Components/MainArea/Bookshelf/Bookshelf.tsx
@@ -1,22 +1,19 @@
 import React from 'react';
-import { BooksConsumer } from '../../../ContextProvider/BooksProvider';
-import { Bookcase } from './Bookcase/Bookcase';
+import {BooksConsumer} from '../../../ContextProvider/BooksProvider';
+import {Bookcase} from './Bookcase/Bookcase';
 
 const Bookshelf = () => (
     <BooksConsumer>
         {context => {
             {
-                const { books } = context;
-                if (books) {
-                    return (
-                        <>
-                            {books!.valueSeq().map(book => (
-                                <Bookcase book={book} key={book.id} />
-                            ))}
-                        </>
-                    );
-                }
-                return <div>load</div>;
+                const {books} = context;
+                return (
+                    <>
+                        {books.valueSeq().map(book => (
+                            <Bookcase book={book} key={book.id}/>
+                        ))}
+                    </>
+                );
             }
         }}
     </BooksConsumer>
