7a5a7918089b94910f870fb846e158ef307bd3d3
Pawel Kochanek <pawel.kochanek@gmail.com>
2019-02-08 19:12:53 +0100

    Book status functionality.

---
+++ src/Components/MainArea/SingleBook/NoBook.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const NoBook = styled.div`
+    font-size: 3rem;
+    padding: 1rem;
+    color: red;
+`;
--- src/Components/MainArea/SingleBook/SingleBook.tsx
+++ src/Components/MainArea/SingleBook/SingleBook.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import { BooksConsumer } from '../../../ContextProvider/BooksProvider';
 import { Bookcase } from '../Bookshelf/Bookcase/Bookcase';
+import { NoBook } from './NoBook';
 
 interface ILink {
     bookId: string;
 }
+
 interface IProps extends RouteComponentProps<ILink> {}
 
 export const SingleBook = (props: IProps) => {
@@ -14,12 +16,12 @@ export const SingleBook = (props: IProps) => {
         <BooksConsumer>
             {context => {
                 {
-                    const { books } = context;
-                    if (books) {
-                        const book = books.get(bookId);
-                        return <Bookcase book={book!} key={book!.id} />;
+                    const { archivedBooks } = context;
+                    const archivedBook = archivedBooks.get(bookId);
+                    if (archivedBook) {
+                        return <Bookcase book={archivedBook} key={archivedBook.id} />;
                     }
-                    return <div>load</div>;
+                    return <NoBook>There is no book with that id!</NoBook>;
                 }
             }}
         </BooksConsumer>
