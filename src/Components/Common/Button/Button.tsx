ae7239b46881fed839be270ca9943c3f087c5007
Pawel Kochanek <pawel.kochanek@gmail.com>
2019-02-09 08:38:51 +0100

    Refactor

---
+++ src/Components/Common/Button/Button.tsx
@@ -0,0 +1,39 @@
+import React, { ReactNode } from 'react';
+import styled from 'styled-components';
+
+interface IButton {
+    color: string;
+    children: ReactNode;
+    className: string;
+    onClick: () => MouseEvent;
+    [key: string]: ReactNode | string;
+}
+
+export const Button = (props: IButton) => {
+    const { children, className, onClick } = props;
+    return (
+        <button className={className} onClick={onClick}>
+            {children}
+        </button>
+    );
+};
+
+export const StyledButton = styled(Button)`
+    background-color: ${({ color }: IButton) => (color ? color : 'gray')};
+    color: white;
+    font-weight: bold;
+    padding: 0.5rem;
+    border: 0;
+    border-radius: 5px;
+    margin-right: 0.5rem;
+
+    &:last-child {
+        margin-right: 0;
+    }
+
+    &:hover {
+        cursor: pointer;
+        background-color: ${({ color }: IButton) => (color ? '#FFCD64' : 'darkgrey')};
+        color: black;
+    }
+`;
