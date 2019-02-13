ed3aa012ef1dc8137e8801f3c5fff48eb2bd4cdf
Pawel Kochanek <pawel.kochanek@gmail.com>
2019-02-13 18:07:04 +0100

    Added tests

--- src/Components/MainArea/Archive/Archive.tsx
+++ src/Components/MainArea/Archive/Archive.tsx
@@ -7,7 +7,7 @@ const Archive = () => (
     <BooksConsumer>
         {context => {
             {
-                const {archivedBooks} = context;
+                const { books } = context;
                 return (
                     <>
                         <FirstRow>
@@ -15,9 +15,12 @@ const Archive = () => (
                             <Author>Author</Author>
                             <ISBN>ISBN</ISBN>
                         </FirstRow>
-                        {archivedBooks.valueSeq().map(book => (
-                            <ArchivedBook book={book} key={book.id}/>
-                        ))}
+                        {books
+                            .valueSeq()
+                            .filter(book => book.isArchived === true)
+                            .map(book => (
+                                <ArchivedBook book={book} key={book.id} />
+                            ))}
                     </>
                 );
             }
