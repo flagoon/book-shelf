8ee5602dea9262265d8c984dc85563b0a5968a11
Pawel Kochanek <pawel.kochanek@gmail.com>
2019-02-15 18:49:56 +0100

    Move Add Book to every page.

--- src/Components/MainArea/MainArea.styled.js
+++ src/Components/MainArea/MainArea.styled.js
@@ -1,6 +1,17 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const StyledMainArea = styled.div`
-    border: 10px solid red;
+    border: 10px red solid;
     width: 820px;
+`;
+
+export const AddBookButton = styled.div`
+    width: 10rem;
+    padding: 1rem;
+    margin: 0.5rem 0 0.5rem 2rem;
+    background-color: darkgreen;
+    border-radius: 3px;
+    color: white;
+    font-size: 1.5rem;
+    font-weight: bold;
 `;
--- src/Components/MainArea/MainArea.tsx
+++ src/Components/MainArea/MainArea.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import {Link, Route, Switch} from 'react-router-dom';
 import { NoMatch } from '../Common/ErrorPages/404_page';
 import AddBookForm from './AddBookForm/AddBookForm';
 import Archive from './Archive/Archive';
 import Bookshelf from './Bookshelf/Bookshelf';
-import { StyledMainArea } from './MainArea.styled';
+import {AddBookButton, StyledMainArea} from './MainArea.styled';
 import { SingleBook } from './SingleBook/SingleBook';
 
 const MainArea = () => {
     return (
         <StyledMainArea>
+            <Link to='/addbook' style={{ textDecoration: 'none', display: 'inline' }}>
+                <AddBookButton>➕ Add book</AddBookButton>
+            </Link>
             <Switch>
                 <Route exact path='/' component={Bookshelf} />
                 <Route path='/bookshelf' component={Bookshelf} />
