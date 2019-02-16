1f85fcf582ab16db5f9d859ed87a89eca2365deb
Pawel Kochanek <pawel.kochanek@gmail.com>
2019-02-16 07:57:08 +0100

    Corrections to styles.

--- src/Components/MainArea/MainArea.styled.js
+++ src/Components/MainArea/MainArea.styled.js
@@ -1,12 +1,11 @@
 import styled from "styled-components";
 
 export const StyledMainArea = styled.div`
-    border: 10px red solid;
     width: 820px;
 `;
 
 export const AddBookButton = styled.div`
-    width: 10rem;
+    width: 170px;
     padding: 1rem;
     margin: 0.5rem 0 0.5rem 2rem;
     background-color: darkgreen;
