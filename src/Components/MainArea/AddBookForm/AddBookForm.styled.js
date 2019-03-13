8a7153e4bca9798178b5dc5910df6dac5fd42e2a
Pawel Kochanek <pawel.kochanek@gmail.com>
2019-03-13 09:55:46 +0100

    Create notification for new book.

---
+++ src/Components/MainArea/AddBookForm/AddBookForm.styled.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+export const StyledForm = styled.form`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const StyledFormElementContainer = styled.div`
+    display: flex;
+    margin: 0.5rem;
+
+    & label {
+        display: flex;
+        width: 500px;
+
+        & *:not(span) {
+            width: 400px;
+            border: 0;
+            border-bottom: 1px solid black;
+            background-color: white;
+            padding-left: 5px;
+            box-sizing: border-box;
+        }
+
+        & textarea {
+            height: 100px;
+        }
+
+        & input {
+            margin-left: 5px;
+        }
+
+        & select {
+            padding: 0;
+        }
+        
+        & span {
+            margin: 5px auto 5px 5px;
+        }
+    }
+`;
