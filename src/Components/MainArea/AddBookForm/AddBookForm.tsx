622263643d4031f66e22b5caa11090e7b8197781
Pawel Kochanek <pawel.kochanek@gmail.com>
2019-03-15 19:03:35 +0100

    Formik implementation tests

--- src/Components/MainArea/AddBookForm/AddBookForm.tsx
+++ src/Components/MainArea/AddBookForm/AddBookForm.tsx
@@ -1,121 +1,35 @@
-import React, {FormEvent} from 'react';
-import { BooksConsumer } from '../../../ContextProvider/BooksProvider';
-import { StyledButton } from '../../Common/Button/Button';
-import { StyledForm, StyledFormElementContainer } from './AddBookForm.styled';
+import { Field, FieldProps, Form, Formik } from 'formik';
+import React from 'react';
+import { StyledFormElementContainer } from './AddBookForm.styled';
 
 interface IAddBookFormProps {
     hideNotification: () => void;
 }
 
-const AddBookForm = ({hideNotification}: IAddBookFormProps) => {
+const AddBookForm = ({ hideNotification }: IAddBookFormProps) => {
     return (
-        <BooksConsumer>
-            {(context) => {
-                const { title, author, picture, pages, date, isbn, description } = context.newBook;
-                const { onNewBookFormSubmit } = context;
-                const submitWithHideNotification = (e: FormEvent<HTMLFormElement>): void | undefined => {
-                    e.preventDefault();
-                    onNewBookFormSubmit(e);
-                    hideNotification();
-                };
-                return (
-                    <StyledForm onSubmit={submitWithHideNotification}>
-                        <StyledFormElementContainer>
-                            <label htmlFor={'title'}>
-                                <span>Title:</span>
-                                <input
-                                    type={'text'}
-                                    id={'title'}
-                                    name={'title'}
-                                    value={title}
-                                    onChange={context.onFormValueChange}
-                                />
-                            </label>
-                        </StyledFormElementContainer>
-                        <StyledFormElementContainer>
-                            <label htmlFor={'author'}>
-                                <span>Author:</span>
-                                <input
-                                    type={'text'}
-                                    id={'author'}
-                                    name={'author'}
-                                    value={author}
-                                    onChange={context.onFormValueChange}
-                                />
-                            </label>
-                        </StyledFormElementContainer>
-                        <StyledFormElementContainer>
-                            <label htmlFor="cover">
-                                <span>Cover:</span>
-                                <select name="cover" id="cover" onChange={context.onFormValueChange}>
-                                    <option value={'hard'}>hard</option>
-                                    <option value={'soft'}>soft</option>
-                                </select>
-                            </label>
-                        </StyledFormElementContainer>
-                        <StyledFormElementContainer>
-                            <label htmlFor={'picture'}>
-                                <span>Picture (url):</span>
-                                <input
-                                    type={'url'}
-                                    id={'picture'}
-                                    name={'picture'}
-                                    value={picture}
-                                    onChange={context.onFormValueChange}
-                                />
-                            </label>
-                        </StyledFormElementContainer>
-                        <StyledFormElementContainer>
-                            <label htmlFor={'pages'}>
-                                <span>Pages:</span>
-                                <input
-                                    type={'number'}
-                                    id={'pages'}
-                                    name={'pages'}
-                                    value={pages}
-                                    onChange={context.onFormValueChange}
-                                />
-                            </label>
-                        </StyledFormElementContainer>
-                        <StyledFormElementContainer>
-                            <label htmlFor={'date'}>
-                                <span>Date:</span>
-                                <input
-                                    type={'date'}
-                                    id={'date'}
-                                    name={'date'}
-                                    value={date}
-                                    onChange={context.onFormValueChange}
-                                />
-                            </label>
-                        </StyledFormElementContainer>
-                        <StyledFormElementContainer>
-                            <label htmlFor={'isbn'}>
-                                <span>ISBN:</span>
-                                <input
-                                    type={'text'}
-                                    id={'isbn'}
-                                    name={'isbn'}
-                                    value={isbn}
-                                    onChange={context.onFormValueChange}
-                                />
-                            </label>
-                        </StyledFormElementContainer>
-                        <StyledFormElementContainer>
-                            <label htmlFor={'description'}>
-                                <span>Description:</span>
-                                <textarea id={'description'} name={'description'} onChange={context.onFormValueChange}>
-                                    {description}
-                                </textarea>
-                            </label>
-                        </StyledFormElementContainer>
-                        <StyledButton color={'blue'} type={'submit'}>
-                            Submit
-                        </StyledButton>
-                    </StyledForm>
-                );
+        <Formik
+            initialValues={{ firstName: '' }}
+            onSubmit={(values: any, actions: any) => {
+                alert(JSON.stringify(values, undefined, 2));
+                actions.setSubmitting(false);
             }}
-        </BooksConsumer>
+            render={() => (
+                <Form>
+                    <StyledFormElementContainer>
+                        <Field
+                            name="firstName"
+                            render={({ field, form }: FieldProps<any>) => (
+                                <div>
+                                    <input type="text" {...field} placeholder="First Name" />
+                                    {form.touched.firstName && form.errors.firstName && form.errors.firstName}
+                                </div>
+                            )}
+                        />
+                    </StyledFormElementContainer>
+                </Form>
+            )}
+        />
     );
 };
 
