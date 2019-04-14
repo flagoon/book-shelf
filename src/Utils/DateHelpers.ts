f5ffc711b9ed02625d71189648d5bf734903d783
Paweł Kochanek <pawel.kochanek@gmail.com>
2019-04-14 21:53:07 +0200

    Corrected function for converting date.

--- src/Utils/DateHelpers.ts
+++ src/Utils/DateHelpers.ts
@@ -1,7 +1,7 @@
 export const convertDateForInputField = (date: Date) => {
     return date
         .toLocaleDateString()
-        .split('.')
+        .split('/')
         .reverse()
         .join('-');
 };
